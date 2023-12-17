1fc94eb802e17ac6b23d30f6ffbbbdc1996fcb00
SailaabTirkey <sailaab.tirkey@altudo.co>
2023-12-17 15:14:38 +0530

    detail page styling

--- src/sxastarter/src/components/BooksDetail/BooksDetail.tsx
+++ src/sxastarter/src/components/BooksDetail/BooksDetail.tsx
@@ -20,12 +20,16 @@ export const Default = (props: BooksDetailProps): JSX.Element => {
   return (
     <div className={`component`}>
       <div className="component-content">
-        <h2 className='text-center'>Book Detail</h2>
+        <h2 className='text-center mb-4 text-decoration-underline'>Book Detail</h2>
         <div className="container">
           <div className="row">
-            <div className="col-md-6 border-right"><img src={props.fields.Image.value?.src}/></div>
+            <div className="col-md-6 text-center p-3"><img src={props.fields.Image.value?.src}/></div>
             <div className="col-md-6">
-              <p>{props.fields.Title.value}</p>
+              <p className='text-decoration-underline font-weight-bold'>{props.fields.Title.value}</p>
+              <br />
+              <p>{props.fields.Description.value}</p>
+              <p>Price: {props.fields.Price.value}</p>
+              <p>Availability: <span className='text-success'>{props.fields.Availability.value}</span></p>
             </div>
           </div>
         </div>
