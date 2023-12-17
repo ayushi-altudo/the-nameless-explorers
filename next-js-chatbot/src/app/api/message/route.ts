e12a8a04b6767a40bfc6cf8b173094a9b4e36d01
AyushiPorwal <ayushi.porwal@altudo.co>
2023-12-17 18:11:17 +0530

    Changes

--- next-js-chatbot/src/app/api/message/route.ts
+++ next-js-chatbot/src/app/api/message/route.ts
@@ -1,26 +1,30 @@
-import { chatbotPrompt } from '../../helpers/constants/chatbot-prompt';
-import { ChatGPTMessage, OpenAIStream, OpenAIStreamPayload } from '../../lib/openai-stream';
-import { MessageArraySchema } from '../../lib/validators/message';
+import { chatbotPrompt } from "@/helpers/constants/chatbot-prompt";
+import {
+  ChatGPTMessage,
+  OpenAIStream,
+  OpenAIStreamPayload,
+} from "@/lib/openai-stream";
+import { MessageArraySchema } from "@/lib/validators/message";
 
-export default async function POST(req: Request) {
+export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const parsedMessages = MessageArraySchema.parse(messages);
 
   const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => {
     return {
-      role: message.isUserMessage ? 'user' : 'system',
+      role: message.isUserMessage ? "user" : "system",
       content: message.text,
     };
   });
 
   outboundMessages.unshift({
-    role: 'system',
+    role: "system",
     content: chatbotPrompt,
   });
 
   const payload: OpenAIStreamPayload = {
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     messages: outboundMessages,
     temperature: 0.4,
     top_p: 1,
---
+++ next-js-chatbot/src/helpers/constants/book-data.ts
@@ -0,0 +1,44 @@
+export const bookData = `
+<url>
+<loc>http://localhost:3000/</loc>
+<desc>Your book store</desc>
+</url>
+<url>
+<loc>http://localhost:3000/catalogue</loc>
+<desc>An overview about our book collection</desc>
+</url>
+<url>
+<loc>http://localhost:3000/catalogue/travel</loc>
+<content>
+    <book>
+    <url>http://localhost:3000/catalogue/Travel/A book about travelling the himalayas</url>
+    <desc>A book about travelling the himalayas</desc>
+    <price>45.17</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>http://localhost:3000/catalogue/Travel/A book by Rick Ansonson about his journey to Mount Ararat</url>
+    <desc>A book by Rick Ansonson about his journey to Mount Ararat</desc>
+    <price>49.43</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>
+<url>
+<loc>http://localhost:3000/catalogue/mystery</loc>
+<content>
+    <book>
+    <url>http://localhost:3000/catalogue/Mystery/Sharp-Objects</url>
+    <desc>A book about by Gillian Flynn about Camille Preaker's troubled past</desc>
+    <price>47.87</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>http://localhost:3000/catalogue/Mystery/The Past Never Ends</url>
+    <desc>A book by Jackson Burnett about the attourney Chester Morgan. His friend died in an unexplained accidental death.</desc>
+    <price>56.50</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>
+`
